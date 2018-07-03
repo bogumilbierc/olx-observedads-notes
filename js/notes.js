914de5ebbcaa8a13b51fd689690f9cd979d1a6a1
Bogumił Bierć <bogumil.bierc@gmail.com>
2018-07-03 19:29:41 +0200

    Storing data to chrome storage and loading it back.

--- js/notes.js
+++ js/notes.js
@@ -1,22 +1,31 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.greeting == "olx") {
-            init();
+            loadDataFromChromeStorage();
         }
     });
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
-    init();
+    loadDataFromChromeStorage();
 });
 
+var notesForAds = {};
 
 $(document).ready(function () {
-    init();
+    loadDataFromChromeStorage();
 });
 
-
-function init() {
-    addNotesButtonToAllAdds();
+function loadDataFromChromeStorage() {
+    chrome.storage.sync.get("notesForAds", function (obj) {
+        if (obj !== null && obj.hasOwnProperty('notesForAds')) {
+            var hatedKeywordsJSON = obj['notesForAds'];
+            notesForAds = JSON.parse(hatedKeywordsJSON);
+            console.log('OLX Observed ads notes: Settings loaded');
+            addNotesButtonToAllAdds();
+        } else {
+            addNotesButtonToAllAdds();
+        }
+    });
 }
 
 function addNotesButtonToAllAdds() {
@@ -30,29 +39,50 @@ function addNotesButtonToAllAdds() {
 
 function addNotesButtonToAdd(addListItem) {
     const favtabDiv = addListItem.querySelector('.favtab.br3.abs.zi4.observelinkgallery');
+    const addId = $(addListItem).data('adid');
     $(favtabDiv).prepend('<img class="custom-notes-button" style="height: 20px;position: absolute;right: 20px;" src="' + chrome.runtime.getURL('/icons/32.png') + '"/>');
 
     const notesButton = favtabDiv.querySelector(".custom-notes-button");
 
     $(notesButton).click(function () {
-        openNotesWidget($(addListItem).data('adid'));
+        openNotesWidget(addId);
     });
-}
 
-function openNotesWidget(adid) {
-    const adLi = $("li.observedad[data-adid=" + adid + "]");
-    var textareaForAd = $("textarea[data-adid=" + adid + "]");
-    if (!textareaForAd || textareaForAd.length === 0) {
-        adLi.append('<textarea data-adid="' + adid + '" style="resize: auto; width: 95%; margin-bottom: 35px"></textarea>');
-        $(adLi).css("height", "auto");
-        textareaForAd = $("textarea[data-adid=" + adid + "]");
-
-        $(textareaForAd).focusout(function () {
-            storeDataForAd(textareaForAd);
-        });
+    if (notesForAds[addId]) {
+        openNotesWidget(addId);
     }
 }
 
-function storeDataForAd(textarea) {
-    console.log('text: ' + textarea.val());
+function openNotesWidget(adid) {
+    const adLi = getAdListItemForAdId(adid);
+    var textareaForAd = $("textarea[data-adid=" + adid + "]");
+    if (!textareaForAd || textareaForAd.length === 0) {
+        textareaForAd = $.parseHTML('<textarea data-adid="' + adid + '" style="resize: auto; width: 95%; margin-bottom: 35px"></textarea>');
+        adLi.append(textareaForAd);
+        $(adLi).css("height", "auto");
+
+        $(textareaForAd).focusout(function () {
+            notesForAds[adid] = $(textareaForAd).val();
+            storeDataInChromeStorage();
+        });
+    }
+    if (notesForAds[adid]) {
+        $(textareaForAd).val(notesForAds[adid]);
+    }
+}
+
+function storeDataInChromeStorage() {
+
+    const jsonToSave = JSON.stringify(notesForAds);
+
+    // Save it using the Chrome extension storage API.
+    chrome.storage.sync.set({'notesForAds': jsonToSave}, function () {
+        // Notify that we saved.
+        console.log('OLX Observed ads notes: Settings saved');
+    });
+}
+
+
+function getAdListItemForAdId(adId) {
+    return $("li.observedad[data-adid=" + adId + "]");
 }
